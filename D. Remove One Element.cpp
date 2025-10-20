8e9b63b62e76b537ffc2f42c6005716f507e6075
KJ0905 <kapsm234@gmail.com>
2025-10-20 17:58:00 +0530

    Codeforces Round 605 (Div. 3)

---
+++ D. Remove One Element.cpp
@@ -0,0 +1,66 @@
+#include<bits/stdc++.h>
+//#include <math>
+//#include <string>
+using namespace std;
+#define ll long long
+#define ld long double
+#define forn(i,j,m) for(int i=j;i<m;i++)
+int mini=INT_MAX;
+int maxi=INT_MIN;
+ll lmini=INT64_MAX;
+ll lmaxi=INT64_MIN;
+#define mod 1000000007
+#define inf 1e9
+//freopen("guess.in","r",stdin);   freopen("billboard.out","w",stdout);
+#define all(x) (x).begin(), (x).end()  //sort(all(vec)) instead of sort(vec.begin(), vec.end()).
+#define al(x,n) x,x+n
+//#define rall(x) (x).rbegin(), (x).rend()
+#define KUNAL ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+#define pb push_back 
+#define getsubstr(x,startingindex,length) x.substr(startingindex,length)
+#define getsubst(x,startingindex) x.substr(startingindex) // to get substring from startingindex upto end.
+// Conversion of int,float into string using to_string(intvalue)
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+typedef __gnu_pbds::tree<int, __gnu_pbds::null_type, less<int>, __gnu_pbds::rb_tree_tag, __gnu_pbds::tree_order_statistics_node_update> ordered_set;
+#define bitindll(val) __builtin_ctzll(val) 
+// __builtin_ctzll(val) (val!=0) is a compiler-specific built-in function that counts the number of trailing zero bits in a long long integer val ,
+//if val=16 then it returns 4 because there are four zero bits after the least significant set bit. 
+#define bitind(val) __builtin_ctz(val) //for unsigned int (val!=0)
+#define countsetbitsll(val) __builtin_popcountll(val) //Counts the number of 1s in the binary representation of a long long integer.
+// __builtin_popcount(unsigned int) for unsigned int
+// __builtin_popcountl(unsigned long) for unsigned long
+
+
+void solve(){
+   int n; cin>>n;
+   vector<int>a(n+1);
+   forn(i,1,n+1){cin>>a[i];}
+   vector<vector<int>>dp(2,vector<int>(n+1));
+   dp[0][1]=1,dp[1][1]=1;
+   forn(i,2,n+1){
+      if(a[i]>a[i-1]){dp[0][i]=dp[0][i-1]+1;}
+      else{dp[0][i]=1;}
+      if(a[i]>a[i-1] && a[i]>a[i-2]){
+         dp[1][i]=max(dp[1][i-1]+1,dp[0][i-2]+1);
+      }
+      else if(a[i]>a[i-1] && a[i]<a[i-2]){
+         dp[1][i]=max(dp[1][i-1]+1,1);
+      }
+      else if(a[i]<a[i-1] && a[i]>a[i-2]){
+         dp[1][i]=max(1,dp[0][i-2]+1);
+      }
+      else if(a[i]<a[i-1] && a[i]<a[i-2]){
+         dp[1][i]=1;
+      }
+   }
+   int ans=INT_MIN;
+   forn(i,1,n+1){
+      ans=max({dp[0][i],dp[1][i],ans});
+   }
+   cout<<ans<<endl;
+}
+int main(){
+    KUNAL
+    solve();
+}
